ae6f7e0031cb1defcabe4247abad9061bd9bee46
Dachao <dugu108@gmail.com>
2013-06-03 14:20:43 +0800

    refactor monitor code

--- lib/cfmonitor.rb
+++ lib/cfmonitor.rb
@@ -4,8 +4,6 @@ module Console
 	class CFMonitor
 		HEALTH_SUFFIX="/healthz"
 		HEALTH_DOWN = "down"
-		NATS_REUQUEST_INTERVAL = 60
-		HEALTH_COLLECT_INTERLVAL = 10
 
 		attr_reader :nats_uri
 		attr_reader :uaa, :uaaIndex, :uaaHostHolder
@@ -13,34 +11,35 @@ module Console
 		attr_reader :router, :routerIndex, :routerHostHolder
 		attr_reader :cc, :ccIndex, :ccHostHolder
 		attr_reader :login, :loginIndex, :loginHostHolder
-		attr_reader :components
+
+		@@components = Hash.new
+		@@components_cache = nil
 
 		def initialize(nats_uri)
 			@nats_uri = nats_uri
 			initProperties
+			initComponentsInfo
 		end
 
-		def getHealth
-			initComponentsInfo
-			collectConponentsHealth
-			@components
+		def CFMonitor.components
+			@@components_cache
 		end
 
 		private
 
 		def initComponentsInfo
-					["TERM", "INT"].each { |sig| trap(sig) { NATS.stop } }
+			["TERM", "INT"].each { |sig| trap(sig) { NATS.stop } }
 
-					NATS.on_error { |err| puts "Server Error: #{err}"; exit! }
+			NATS.on_error { |err| puts "Server Error: #{err}"; exit! }
 
-					NATS.start(:uri => @nats_uri) {
-						NATS.request('vcap.component.discover') { |response|
-							puts "Got response for Components: '#{response}'"
-							sleep(0.5)
-							NATS.stop
-							parseDiscoverResponse(response)
-						}
-					}
+			NATS.start(:uri => @nats_uri) {
+				nid = NATS.request('vcap.component.discover') { |response|
+					puts "Got response for Components: '#{response}'"
+					NATS.stop
+					sleep(0.2)
+					parseDiscoverResponse(response)
+				}
+			}
 		end
 
 		def initProperties
@@ -58,7 +57,6 @@ module Console
 
 			@uaaIndex=@deaIndex=@routerIndex=@loginIndex=@ccIndex=0
 
-			@components = Hash.new
 			@components["uaa"] = @uaa
 			@components["dea"] = @dea
 			@components["login"] = @login
@@ -69,8 +67,10 @@ module Console
 		def parseDiscoverResponse(response)
 			response.each_line { |line|
 				component = parseJsonToComponent(line)
-				specifyComponent(component)
+				type = specifyComponent(component)
 			}
+			collectConponentsHealth(type)
+			@@components_cache = @@components
 		end
 
 		def parseJsonToComponent (json_str)
@@ -78,12 +78,13 @@ module Console
 		end
 
 		def specifyComponent (info)
+			type = nil;
 			case info["type"].downcase
 				when "uaa" then
 					if !@uaaHostHolder.include? (info["host"] + "/uaa")
 						@uaa[@uaaIndex]=info.clone
 						@uaa[@uaaIndex]["host"] = @uaa[@uaaIndex]["host"] + "/uaa"
-						@uaa[@uaaIndex]["purehost"] =  retriveHost info["host"]
+						@uaa[@uaaIndex]["purehost"] = retriveHost info["host"]
 						@uaaHostHolder.push @uaa[@uaaIndex]["host"]
 						@uaaIndex+=1
 
@@ -93,106 +94,90 @@ module Console
 						@login[@loginIndex]["purehost"] = retriveHost info["host"]
 						@loginHostHolder.push @login[@loginIndex]["host"]
 						@loginIndex+=1
-
+						type = "uaa"
 					end
 				when "dea" then
 					if !@deaHostHolder.include? info["host"]
 						@dea[@deaIndex]=info
-						@dea[@deaIndex]["host"] =  @dea[@deaIndex]["host"]
+						@dea[@deaIndex]["host"] = @dea[@deaIndex]["host"]
 						@dea[@deaIndex]["purehost"] = retriveHost info["host"]
 						@deaHostHolder.push @dea[@deaIndex]["host"]
 						@deaIndex+=1
+						type = "dea"
 					end
 				when "router" then
 					if !@routerHostHolder.include? info["host"]
 						@router[@routerIndex]=info
-						@router[@routerIndex]["host"] =  @router[@routerIndex]["host"]
+						@router[@routerIndex]["host"] = @router[@routerIndex]["host"]
 						@router[@routerIndex]["purehost"] = retriveHost info["host"]
 						@routerHostHolder.push @router[@routerIndex]["host"]
 						@routerIndex+=1
+						type = "router"
 					end
 				when "cloudcontroller" then
 					if !@ccHostHolder.include? info["host"]
 						@cc[@ccIndex]=info
-						@cc[@ccIndex]["host"] =  @cc[@ccIndex]["host"]
+						@cc[@ccIndex]["host"] = @cc[@ccIndex]["host"]
 						@cc[@ccIndex]["purehost"] = retriveHost info["host"]
 						@ccHostHolder.push @cc[@ccIndex]["host"]
 						@ccIndex+=1
+						type = "cc"
 					end
 				else
 					raise "Cannot parse response" + info
 			end
+			type
 		end
 
-		def collectConponentsHealth
-			(0..@uaaIndex-1).each { |i|
-				updateHealth(@uaa[i])
-			}
-			(0..@loginIndex-1).each { |i|
-				updateHealth(@login[i])
-			}
-			(0..@deaIndex-1).each { |i|
-				updateHealth(@dea[i])
-			}
-			(0..@ccIndex-1).each { |i|
-				updateHealth(@cc[i])
-			}
-			(0..@routerIndex-1).each { |i|
-				updateHealth(@router[i])
-			}
-
-		end
-
-		def createHealthzUrl (host, credential)
-			"http://" + credential[0]+":"+credential[1] + "@" + host + HEALTH_SUFFIX
-		end
-
-		def updateHealth(com)
-			url = createHealthzUrl(com["host"], com["credentials"])
-			result = `curl -s #{url}`
-			if $?.to_i == 0
-				if com["type"] == "Router"
-					com["health"]=parseJsonToComponent(result)["health"]
-				else
-					com["health"]=result
-				end
-
-			else
-				com["health"]=HEALTH_DOWN
+		def collectConponentsHealth(type)
+			case info["type"].downcase
+				when "uaa" then
+					(0..@uaaIndex-1).each { |i|
+						updateHealth(@uaa[i])
+					}
+					(0..@loginIndex-1).each { |i|
+						updateHealth(@login[i])
+					}
+				when "dea" then
+					(0..@deaIndex-1).each { |i|
+						updateHealth(@dea[i])
+					}
+				when "cc" then
+					(0..@ccIndex-1).each { |i|
+						updateHealth(@cc[i])
+					}
+				when "router" then
+					(0..@routerIndex-1).each { |i|
+						updateHealth(@router[i])
+					}
 			end
+		else
+			raise "Cannot collect health " + type
 		end
 
-		def retriveHost(hostWithPort)
-			hostWithPort.slice(0, hostWithPort.index(":"))
+	end
+
+	def createHealthzUrl (host, credential)
+		"http://" + credential[0]+":"+credential[1] + "@" + host + HEALTH_SUFFIX
+	end
+
+	def updateHealth(com)
+		url = createHealthzUrl(com["host"], com["credentials"])
+		result = `curl -s #{url}`
+		if $?.to_i == 0
+			if com["type"] == "Router"
+				com["health"]=parseJsonToComponent(result)["health"]
+			else
+				com["health"]=result
+			end
+
+		else
+			com["health"]=HEALTH_DOWN
 		end
+	end
+
+	def retriveHost(hostWithPort)
+		hostWithPort.slice(0, hostWithPort.index(":"))
 	end
 end
 
-#monitor = Console::Monitor.new("nats://10.168.3.189:4222")
-#result = Thread.new do
-#	loop {
-#		puts "------------------------------------------------------------"
-#		monitor.uaaIndex.times { |i|
-#			puts "UAA " + i.to_s + " : " + (monitor.uaa[i].to_s|| "nil")
-#		}
-#
-#		monitor.loginIndex.times { |i|
-#			puts "LOGIN " + i.to_s + " : " + (monitor.login[i].to_s|| "nil")
-#		}
-#
-#		monitor.deaIndex.times { |i|
-#			puts "DEA " + i.to_s + " : " + (monitor.dea[i].to_s|| "nil")
-#		}
-#
-#		monitor.routerIndex.times { |i|
-#			puts "ROUTER " + i.to_s + " : " + (monitor.router[i].to_s|| "nil")
-#		}
-#
-#		monitor.ccIndex.times { |i|
-#			puts "CC " + i.to_s + " : " + (monitor.cc[i].to_s|| "nil")
-#		}
-#
-#		sleep(10)
-#	}
-#end
-#result.join()
--- lib/server.rb
+++ lib/server.rb
@@ -7,229 +7,236 @@ require 'fileutils'
 
 class Server < Sinatra::Base
 
-  include Console
+	include Console
 
-  set :root, File.expand_path('../../', __FILE__)
+	set :root, File.expand_path('../../', __FILE__)
 
-  before do
-    @access_token = request.cookies["access_token"]
-    if @access_token
-      begin
-        @client = cloudfoundry_client(nil, CFoundry::AuthToken.new(@access_token))
-        @current_user = @client.current_user
-        login = @client.logged_in?
-      rescue Exception => e
-          puts e
-          login = false
-      end
-    else
-      login = false
-    end
+	begin
+		@@config = YAML.load_file("config/test.yml")
+	rescue => e
+		abort "ERROR: Failed loading config: #{e}"
+	end
 
-    if !login && request.path_info != '/login'
-      redirect to('/login?redirect=true')
+	before do
+		@access_token = request.cookies["access_token"]
+		if @access_token
+			begin
+				@client = cloudfoundry_client(nil, CFoundry::AuthToken.new(@access_token))
+				@current_user = @client.current_user
+				login = @client.logged_in?
+			rescue Exception => e
+				puts e
+				login = false
+			end
+		else
+			login = false
 		end
 
-  end
+		if !login && request.path_info != '/login'
+			redirect to('/login?redirect=true')
+		end
 
-  get '/' do
-    #puts @client.public_methods
-    orgs = @client.organizations_by_user_guid @client.current_user.guid
-    #puts @client.public_methods
+	end
 
-    erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
-      erb :index, :locals => {:orgs => orgs}
-    end
+	get '/' do
+		#puts @client.public_methods
+		orgs = @client.organizations_by_user_guid @client.current_user.guid
+		#puts @client.public_methods
 
-
-  end
-
-  get '/login' do
-    erb :login, :layout => :base
-  end
-
-  post '/login' do
-    username = params['username']
-    password = params['password']
-
-    @client = cloudfoundry_client()
-    access_token = @client.login(username,password)
-    if access_token
-      response.set_cookie("access_token", :value => access_token.auth_header.to_s )
-      redirect to('/')
-    end
-  end
-
-  get '/apps' do
-    orgs = @client.organizations_by_user_guid @client.current_user.guid
-    erb :index, :locals => {:orgs => orgs}
-  end
-
-  get '/app/create' do
-    orgs = @client.organizations_by_user_guid @client.current_user.guid
-    erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
-      erb :create_app , :locals => {:orgs => orgs}
-    end
-
-  end
-
-  post '/app/create' do
-    buildpack = params["buildpack"]
-    name = params["name"]
-    org = params["org"]
-
-    org = @client.organization org
-    space = nil
-    for s in org.spaces
-      if s.name == 'default'
-        space = s
-        break
-      end
-    end
-
-    raise Exception, "error: this org has no default space." if space == nil
-
-    app = @client.app
-    app.name = name
-    app.total_instances = 1 # <- set the number of instances you want
-    app.memory = 128 # <- set the allocated amount of memory
-    #app.buildpack = buildpack
-    app.space = space
-    app.create!
-
-    domain = @client.domain_by_name "limy.cf2.youdao.com"
-    route = @client.route
-    route.host = name
-    route.domain = domain
-    route.space = space
-    route.create!
-
-    app.add_route(route)
-
-    svn = Svn.new(app.guid, buildpack)
-    svn.mkdir_on_remote_svn_server
-    svn.mkdir_on_local
-    app.upload svn.local_app_dir
-
-    app.start!(true)
-
-    redirect to("/app/#{app.guid}")
-  end
+		erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
+			erb :index, :locals => {:orgs => orgs}
+		end
 
 
-  get '/app/:guid' do |guid|
-    app = @client.app guid
-   # puts app.public_methods
-    erb :app , :locals => {:app => app, :current_user => @current_user}
-    erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
-      erb :app , :locals => {:app => app}
-    end
-  end
+	end
 
-  post '/app/:guid' do |guid|
-    app = @client.app guid
+	get '/login' do
+		erb :login, :layout => :base
+	end
 
-    action = params["action"]
+	post '/login' do
+		username = params['username']
+		password = params['password']
 
-    case action
-      when "start"
-        app.start!
-      when "stop"
-        app.stop!
-      when "restart"
-        app.restart!
-      when "update"
-        revision = params["revision"]
+		@client = cloudfoundry_client()
+		access_token = @client.login(username, password)
+		if access_token
+			response.set_cookie("access_token", :value => access_token.auth_header.to_s)
+			redirect to('/')
+		end
+	end
 
-        tmpdir =  "#{Dir.tmpdir}/.apps"
-        FileUtils.mkdir_p tmpdir
-        FileUtils.rm_rf "#{tmpdir}/#{guid}"
-        Dir.chdir tmpdir do |dir|
-          svn = Svn.new(app.guid)
-          result = `svn co #{svn.svn_app_dir}  -r #{revision}  --username limy --password LMYlmy111`
-          raise SvnException, result unless $?.to_i == 0
-          app.upload "#{dir}/#{guid}"
-          logger.info "uploading #{dir}/#{guid}"
-          app.restart!(true)
-        end
+	get '/apps' do
+		orgs = @client.organizations_by_user_guid @client.current_user.guid
+		erb :index, :locals => {:orgs => orgs}
+	end
 
-      else
-        logger.error 'wrong action!'
-    end
+	get '/app/create' do
+		orgs = @client.organizations_by_user_guid @client.current_user.guid
+		erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
+			erb :create_app, :locals => {:orgs => orgs}
+		end
 
-    redirect to("/app/#{guid}")
-  end
+	end
+
+	post '/app/create' do
+		buildpack = params["buildpack"]
+		name = params["name"]
+		org = params["org"]
+
+		org = @client.organization org
+		space = nil
+		for s in org.spaces
+			if s.name == 'default'
+				space = s
+				break
+			end
+		end
+
+		raise Exception, "error: this org has no default space." if space == nil
+
+		app = @client.app
+		app.name = name
+		app.total_instances = 1 # <- set the number of instances you want
+		app.memory = 128 # <- set the allocated amount of memory
+														#app.buildpack = buildpack
+		app.space = space
+		app.create!
+
+		domain = @client.domain_by_name "limy.cf2.youdao.com"
+		route = @client.route
+		route.host = name
+		route.domain = domain
+		route.space = space
+		route.create!
+
+		app.add_route(route)
+
+		svn = Svn.new(app.guid, buildpack)
+		svn.mkdir_on_remote_svn_server
+		svn.mkdir_on_local
+		app.upload svn.local_app_dir
+
+		app.start!(true)
+
+		redirect to("/app/#{app.guid}")
+	end
 
 
+	get '/app/:guid' do |guid|
+		app = @client.app guid
+		# puts app.public_methods
+		erb :app, :locals => {:app => app, :current_user => @current_user}
+		erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
+			erb :app, :locals => {:app => app}
+		end
+	end
 
-  get '/api/orgs' do
-    content_type :json
+	post '/app/:guid' do |guid|
+		app = @client.app guid
 
-    orgs = @client.organizations_by_user_guid @client.current_user.guid
-    #puts orgs[0].public_methods
-    orgs.to_json
-  end
+		action = params["action"]
 
-  get '/api/org/:guid' do |guid|
-    content_type :json
-    org = @client.Organization guid
-    {:guid => org.guid, :name => org.name }.to_json
-  end
+		case action
+			when "start"
+				app.start!
+			when "stop"
+				app.stop!
+			when "restart"
+				app.restart!
+			when "update"
+				revision = params["revision"]
 
-  get '/api/org/:guid/apps' do |guid|
-    content_type :json
+				tmpdir = "#{Dir.tmpdir}/.apps"
+				FileUtils.mkdir_p tmpdir
+				FileUtils.rm_rf "#{tmpdir}/#{guid}"
+				Dir.chdir tmpdir do |dir|
+					svn = Svn.new(app.guid)
+					result = `svn co #{svn.svn_app_dir}  -r #{revision}  --username limy --password LMYlmy111`
+					raise SvnException, result unless $?.to_i == 0
+					app.upload "#{dir}/#{guid}"
+					logger.info "uploading #{dir}/#{guid}"
+					app.restart!(true)
+				end
 
-    spaces = @client.spaces_by_organization_guid "af78c950-6a67-4277-aa27-f2a246f46e0e"
-    apps = @client.apps_by_organization_guid "af78c950-6a67-4277-aa27-f2a246f46e0e"
-    apps.to_json
-  end
+			else
+				logger.error 'wrong action!'
+		end
 
-  get '/api/apps' do
-    nil
+		redirect to("/app/#{guid}")
+	end
+
+
+	get '/api/orgs' do
+		content_type :json
+
+		orgs = @client.organizations_by_user_guid @client.current_user.guid
+		#puts orgs[0].public_methods
+		orgs.to_json
+	end
+
+	get '/api/org/:guid' do |guid|
+		content_type :json
+		org = @client.Organization guid
+		{:guid => org.guid, :name => org.name}.to_json
+	end
+
+	get '/api/org/:guid/apps' do |guid|
+		content_type :json
+
+		spaces = @client.spaces_by_organization_guid "af78c950-6a67-4277-aa27-f2a246f46e0e"
+		apps = @client.apps_by_organization_guid "af78c950-6a67-4277-aa27-f2a246f46e0e"
+		apps.to_json
+	end
+
+	get '/api/apps' do
+		nil
 	end
 
 	get '/monitor' do
-		if @monitor.nil?
-			@monitor = Console::CFMonitor.new("nats://10.168.3.189:4222")
-		end
-		components = @monitor.getHealth
-		erb :monitor, :locals => {:components => components, :current_user => @current_user}
+		monitor = Console::CFMonitor.new(@@config["nats_uri"])
+		redirect to("/monitor/show")
 	end
 
+	get '/monitor/show' do
+		erb :layout, :layout => :base, :locals => {:current_user => @current_user} do
+			erb :monitor, :locals => {:components => Console::CFMonitor.components}
+		end
+	end
 end
 
 
 require "cfoundry/v2/model"
 module CFoundry::V2
-  class Organization < Model
-    def to_json(*a)
-      hash = {
-          :guid => guid,
-          :name => name,
-          :spaces => spaces
-      }
+	class Organization < Model
+		def to_json(*a)
+			hash = {
+					:guid => guid,
+					:name => name,
+					:spaces => spaces
+			}
 
-      hash.to_json
-    end
-  end
+			hash.to_json
+		end
+	end
 
-  class Space < Model
-    def to_json(*a)
-      hash = {
-          :name => name,
-      }
+	class Space < Model
+		def to_json(*a)
+			hash = {
+					:name => name,
+			}
 
-      hash.to_json
-    end
-  end
+			hash.to_json
+		end
+	end
 
-  class App < Model
-    def to_json(*a)
-      hash = {
-          :name => name,
-      }
+	class App < Model
+		def to_json(*a)
+			hash = {
+					:name => name,
+			}
 
-      hash.to_json
-    end
-  end
+			hash.to_json
+		end
+	end
 end
